acaa7ab4be611d66834c909eea41b6b41d1d6a27
the-dude40 <a024908@ismai.pt>
2015-04-14 11:18:32 +0100

    Classlogin versao v1.1

--- src/main/java/projecto1/login.java
+++ src/main/java/projecto1/login.java
@@ -1,5 +1,24 @@
 package projecto1;
 
-public class login {
+import javax.enterprise.context.*;
+import javax.inject.Named;
 
+
+@Named
+@SessionScoped
+public class Login {
+
+	private String name;
+
+	public Login() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String user_name) {
+		this.name = user_name;
+	}
 }
+
