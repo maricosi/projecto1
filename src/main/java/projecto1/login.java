75ab647e1576fe859f3c17c0e315cae841aa77b9
maricosi <marisaisimoes@gmail.com>
2015-04-16 01:22:43 +0100

    Projecto Maven 16/04 01:22 Marisa

--- src/main/java/projecto1/login.java
+++ src/main/java/projecto1/login.java
@@ -1,13 +1,19 @@
 package projecto1;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.*;
 import javax.inject.Named;
 
 
 @Named
 @SessionScoped
-public class login {
+public class login implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String name;
 
 	public login() {
